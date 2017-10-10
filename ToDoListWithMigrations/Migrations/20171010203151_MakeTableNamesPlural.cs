b10bcba47c3f66550d06f869a2a2bba136aa80db
Parul Mishra <parul.mishra78@gmail.com>
2017-10-10 15:52:03 -0700

    initial commit

---
+++ ToDoListWithMigrations/Migrations/20171010203151_MakeTableNamesPlural.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoListWithMigrations.Migrations
+{
+    public partial class MakeTableNamesPlural : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_Item_Category_CategoryId", table: "Item");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Item_Category_CategoryId",
+                table: "Items",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Items");
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_Item_Category_CategoryId", table: "Item");
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Items");
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Item_Category_CategoryId",
+                table: "Items",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ ToDoListWithMigrations/Models/ToDoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoListWithMigrations.Models
+{
+    public class ToDoDbContext: DbContext
+    {
+        public ToDoDbContext()
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=3306;database=ToDoListWithMigrations;uid=parul;pwd=parul;");
+        }
+
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
